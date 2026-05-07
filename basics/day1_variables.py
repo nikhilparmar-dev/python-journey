68dc4e92e89bb3843b971aeacd4c55accefaf832
Nikhil Parmar <nikhilparmarofc@gmail.com>
2026-05-07 15:47:11 +0530

    Pushed python variables and data types code

---
+++ basics/day1_variables.py
@@ -0,0 +1,63 @@
+print("Data Types")
+
+# DataTypes
+
+
+# 1. Integer (Numbers)
+print("\nInteger :  ")
+a = 10
+b = 18
+age = 17
+print(a, b, ", age: ", age)
+
+
+# 2. float (Decimal Numbers)
+print("\nFloat : ")
+percentage = 66.57
+height = 5.6
+weight = 48.18
+print("Percentage : ", percentage)
+print("Height : ", height)
+print("Weight : ", weight)
+
+
+# 3. String (Text)
+print("\nString : ")
+name = "Nikhil"
+surname = 'Parmar'
+print("Full Name = ", name, surname)
+
+
+# 4. Booleans (True or false)
+print("\nBoolean : ")
+isAdult = True
+isTopper = False
+print("18+ Hai ? : ", isAdult)
+print("Topper Hai ? : ", isTopper)
+
+
+# 5. List (Same cataory Data - Mutable)
+print("\nList : ")
+marks = [18, 19, 20]
+boys = ["Nikhil", "Prem", "Zeeshan"]
+print("Students : ", boys)
+print("Marks : ", marks)
+
+
+# 6. Tuple (Immutable)
+print("\nTuple : ")
+Marks = (99, 45, 18, 67, (12, 13, 24))
+print("Marks : ", Marks)
+
+
+#7. Dictionary (Key-Value Pair)
+print("\nDictionary : ")
+
+student = {
+'Name' : 'Nikhil',
+'Age' : 17,
+'City' : 'Kadi'
+}
+
+print("Student Details: ")
+print(student)
