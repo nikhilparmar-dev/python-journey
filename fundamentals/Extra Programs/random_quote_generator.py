3ba3bff20f1490d0065347e51e62fb48fe16ac00
Nikhil R. Parmar <nikhilparmarofc@gmail.com>
2026-06-02 21:59:31 +0530

    Create random_quote_generator.py

---
+++ fundamentals/Extra Programs/random_quote_generator.py
@@ -0,0 +1,11 @@
+import random
+
+quotes = [
+    "Consistency beats talent.",
+    "Keep learning every day.",
+    "Small progress is still progress.",
+    "Discipline creates freedom.",
+    "Focus on the process."
+]
+
+print(random.choice(quotes))
